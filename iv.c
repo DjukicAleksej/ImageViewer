dc5a99dbfbd6778d7643206a206350b2e8bbeb3b
DjukicAleksej <aleksejdjukicets@gmail.com>
2025-12-17 16:35:40 +0100

    Trying to make the sdl2 work and open a window

--- iv.c
+++ iv.c
@@ -2,5 +2,14 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h> 
 int main(){
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("SLD-Init Error: %s\n", SDL_GetError());
+        return 1;
+    }
+SDL_Window *pwindow = SDL_CreateWindow("Image Viewer",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,900,600 , SDL_WINDOW_SHOWN);
+if(!pwindow) {
+    printf("SDL_CreateWindow Error: %s\n", SDL_GetError());
+    return 1;
+}
 
 }
